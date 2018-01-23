8c66ed2b48fedacb782b053e238dc5d4c545187c
tracer-ics2017 <tracer@njuics.org>
2018-01-23 22:29:31 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  22:29:31 up 1 day, 19:36,  7 users,  load average: 0.00, 0.00, 0.00 73313ec8ea168b61200eb845e0f3bf29996baeb9

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -108,7 +108,15 @@ static int cmd_info(char *args){
     }
     else{
         if(strcmp(arg,"r")==0){
-            printf("eax: %d\n",cpu.eax);
+            printf("eax: %20d\n",cpu.eax);
+            printf("ecx: %20d\n",cpu.ecx);
+            printf("edx: %20d\n",cpu.edx);
+            printf("ebx: %20d\n",cpu.ebx);
+            printf("esp: %20d\n",cpu.esp);
+            printf("ebp: %20d\n",cpu.ebp);
+            printf("esi: %20d\n",cpu.esi);
+            printf("edi: %20d\n",cpu.edi);
+            printf("ax: %20d\n",reg_w(R_AX));
         }
         else{
             printf("default\n");
