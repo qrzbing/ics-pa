80f06941ca6f9070cb5f7c35ca4b302398d0dc55
tracer-ics2017 <tracer@njuics.org>
2018-02-04 20:32:46 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  20:32:46 up 13 days, 17:40, 12 users,  load average: 0.01, 0.02, 0.00 1242a0fa995c83e6fe31e1a2b50b97c48cb8bc

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -230,7 +230,7 @@ uint32_t eval(uint32_t p,uint32_t q){
                     push(S, '1');
                 }
                 case TK_RPARE: {
-                    if(isEmpty(S)){
+                    if(isEmpty(S) == true){
                         panic("Wrong Expression");
                     }
                     pop(S);
