5e0c16b03f2d75a2fcaa7fab1a61d882c3180554
tracer-ics2017 <tracer@njuics.org>
2018-03-01 12:10:33 +0000

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  12:10:33 up 38 days, 17:17,  5 users,  load average: 0.01, 0.00, 0.00 d8d19f08c8f365b5c850bdb8555d4044033b112

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -25,7 +25,9 @@ make_EHelper(jmp_rm) {
 
 make_EHelper(call) {
   // the target address is calculated at the decode stage
-  TODO();
+  //TODO();
+  decoding.is_jmp = 1;
+  decoding.jmp_eip=id_dest->val;
 
   print_asm("call %x", decoding.jmp_eip);
 }
