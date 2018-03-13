ca8f8984beabfb6fdb8a07d0dc8378376da07cce
tracer-ics2017 <tracer@njuics.org>
2018-03-13 19:23:40 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  19:23:40 up 50 days, 16:31, 15 users,  load average: 0.09, 0.03, 0.00 85d85cb3f2b7d7b3fa2f22ab132a746f6055d3da

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -132,14 +132,14 @@ static int cmd_info(char *args){
         if(strcmp(arg, "r") == 0){
             int temp_count=0;
             for(temp_count = 0; temp_count < 8; ++temp_count){
-                printf("%s:    0x%x\n", regsl[temp_count], reg_l(temp_count));
+                printf("%-5s0x%x\n", regsl[temp_count], reg_l(temp_count));
             }
-            printf("eip:    0x%x\n", cpu.eip);
+            printf("eip  0x%x\n", cpu.eip);
             for(temp_count = 0; temp_count < 8; ++temp_count){
-                printf("%s:     0x%x\n", regsw[temp_count], reg_w(temp_count));
+                printf("%-5s0x%x\n", regsw[temp_count], reg_w(temp_count));
             }
             for(temp_count = 0; temp_count < 8; ++temp_count){
-                printf("%s:     0x%x\n", regsb[temp_count], reg_b(temp_count));
+                printf("%-5s0x%x\n", regsb[temp_count], reg_b(temp_count));
             }
         }
         else if(strcmp(arg, "w") == 0){
