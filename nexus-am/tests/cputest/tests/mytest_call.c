26262425667857d471e8d2c4dfc8cf965a026998
tracer-ics2017 <tracer@njuics.org>
2018-04-04 22:33:30 +0800

    >  run 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  22:33:30 up 72 days, 19:40, 15 users,  load average: 0.02, 0.01, 0.00 12fb2d48bec69c5e172e3302429c03bb805ff621

---
+++ nexus-am/tests/cputest/tests/mytest_call.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: mytest_call.c
+	> Author: Qin Ruizhe
+	> Mail: qrzbing@nuaa.edu.cn
+	> Created Time: Wed Apr  4 22:30:02 2018
+ ************************************************************************/
+
+#include"trap.h"
+void func(){
+    return;
+}
+int main(){
+    func();
+    return 0;
+}
+
