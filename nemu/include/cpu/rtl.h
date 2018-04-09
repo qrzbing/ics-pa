f34694536cf03f4abc6ce8334c39069964b27b55
tracer-ics2017 <tracer@njuics.org>
2018-04-09 16:39:11 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  16:39:11 up 77 days, 13:46, 14 users,  load average: 0.00, 0.02, 0.00 22af83296424859d5766492cb171eff26e06f58

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -138,14 +138,10 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
     //TODO();
     
-    //t0 = *src1;
-    uint32_t ttmp = *src1;
-    printf("t0 = %#x\n", ttmp);
-    ttmp = ttmp << (32 - width * 8);
-    printf("t0 = %#x\n", ttmp);
-    ttmp = ttmp >> (32 - width * 8);
-    printf("t0 = %#x\n", ttmp);
-    *dest = ttmp;
+    int32_t ttemp = (int32_t)(*src1);
+    ttemp = ttemp << (32 - width * 8);
+    ttemp = ttemp >> (32 - width * 8);
+    *dest = ttemp;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
