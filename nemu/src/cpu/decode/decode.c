258f5a1cdd3f4d5056c89b4ea4b2938ed4a3f5b8
tracer-ics2017 <tracer@njuics.org>
2018-04-09 16:57:22 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  16:57:22 up 77 days, 14:04, 14 users,  load average: 0.03, 0.01, 0.00 b908fda9d9b1b7ea39855e29d0583fec9422163

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -39,8 +39,8 @@ static inline make_DopHelper(SI) {
    op->simm = ???
    */
     op->imm = instr_fetch(eip, op->width);
-    rtl_sext(&op->val, &op->imm, op->width);
-    op->simm = op->val;
+    rtl_sext(&t0, &op->imm, op->width);
+    op->simm = t0;
     //op->simm = op->simm << (32 - op->width * 4) >> (32 - op->width);
     rtl_li(&op->val, op->simm);
     
