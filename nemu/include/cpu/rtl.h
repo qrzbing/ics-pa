0bc4308e6ffc00d2a6e2c5dd120b64c0d5586213
tracer-ics2017 <tracer@njuics.org>
2018-04-12 23:45:45 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  23:45:45 up 80 days, 20:53, 14 users,  load average: 0.11, 0.04, 0.01 c68cb4f837dc280c3fd8cc2411c7f479e8d58a1d

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -147,21 +147,17 @@ static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-    //rtl_li(&t0, 0x4);
-    //rtl_sub(&cpu.esp, &cpu.esp, &t0);
-    //rtl_sm(&cpu.esp, t0, src1);
-    cpu.esp -= 4;
-    vaddr_write(cpu.esp, 4, *src1);
+    rtl_li(&t0, 0x4);
+    rtl_sub(&cpu.esp, &cpu.esp, &t0);
+    rtl_sm(&cpu.esp, t0, src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-    //rtl_li(&t0, 0x4);
-    //rtl_lm(dest, &cpu.esp, t0);
-    //rtl_add(&cpu.esp, &cpu.esp, &t0);
-    *dest = vaddr_read(cpu.esp, 4);
-    cpu.esp += 4;
+    rtl_li(&t0, 0x4);
+    rtl_lm(dest, &cpu.esp, t0);
+    rtl_add(&cpu.esp, &cpu.esp, &t0);
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
