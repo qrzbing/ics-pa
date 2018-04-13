8caaea98b636371fef20145a54375144c0e97487
tracer-ics2017 <tracer@njuics.org>
2018-04-13 16:18:24 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  16:18:24 up 81 days, 13:25, 14 users,  load average: 0.07, 0.02, 0.00 3782c5592ca0f999f9de18fa2a56aa56b38d2021

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -187,7 +187,10 @@ static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
     //unsigned int temp = ~0;
     //temp = temp >> (32 - 8 * width);
     //cpu.ZF = (*result & temp) | 0;
-    cpu.ZF = (*result & ~(0xffffffff << (8 * width - 1) << 1)) == 0;
+    //cpu.ZF = (*result & ~(0xffffffff << (8 * width - 1) << 1)) == 0;
+    //rtl_eq0()
+    rtlreg_t temp = ((*result) == 0);
+    rtl_set_ZF(&temp);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
