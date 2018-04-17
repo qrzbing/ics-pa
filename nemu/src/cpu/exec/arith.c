84ef87b3b30a7ba3c1b8a1666e029a5bd47d688f
tracer-ics2017 <tracer@njuics.org>
2018-04-17 18:45:57 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  18:45:57 up 85 days, 15:53,  7 users,  load average: 0.08, 0.02, 0.01 324fe50cd77aa813c874da0e885466e67433ae31

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -63,8 +63,22 @@ make_EHelper(cmp) {
 }
 
 make_EHelper(inc) {
-    rtl_addi(&t1, &id_dest->val, 1);
-    operand_write(id_dest, &t1);
+    rtl_addi(&t0, &id_dest->val, 1);
+    operand_write(id_dest, &t0);
+    
+    rtl_update_ZFSF(&t0, id_dest->width);
+    
+    rtl_sltu(&t1, &t0, &id_dest->val);
+    rtl_sltu(&t2, &t0, &id_src->val);
+    rtl_or(&t1, &t1, &t2);
+    rtl_set_CF(&t1);
+    
+    rtl_xor(&t1, &t0, &id_dest->val);
+    rtl_xor(&t2, &t0, &id_src->val);
+    rtl_and(&t1, &t1, &t2);
+    rtl_msb(&t1, &t1, id_dest->width);
+    rtl_set_OF(&t1);
+
     print_asm_template1(inc);
 }
 
