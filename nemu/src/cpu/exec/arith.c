6717134fd8fb25ad78195915fa65ed5fbfc865dd
tracer-ics2017 <tracer@njuics.org>
2018-04-17 22:28:49 +0800

    >  compile 161620125 QinRuizhe Linux 4c8d54caf9bf 4.4.0-105-generic #128-Ubuntu SMP Thu Dec 14 12:42:11 UTC 2017 x86_64 GNU/Linux  22:28:49 up 85 days, 19:36,  9 users,  load average: 0.07, 0.06, 0.01 aaea8998d473ce18eefc5c4de7df5c34852356ac

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -284,7 +284,7 @@ make_EHelper(idiv) {
       break;
     default: assert(0);
   }
-    printf("test");
+    printf("test7\n");
   rtl_idiv(&t2, &t3, &t1, &t0, &id_dest->val);
 
   rtl_sr(R_EAX, id_dest->width, &t2);
