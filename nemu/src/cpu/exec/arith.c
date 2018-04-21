254da4ab8cf053de87a0bc7ef9599d6477683df8
tracer-ics2017 <tracer@njuics.org>
2018-04-21 18:39:16 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:48:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:39:16 up  3:37,  7 users,  load average: 0.21, 0.15, 0.16 478c13d1687001c433f7cbb666e77cc29ab8a93

--- nemu/src/cpu/exec/arith.c
+++ nemu/src/cpu/exec/arith.c
@@ -96,12 +96,15 @@ make_EHelper(dec) {
     rtl_sltu(&t1, &id_dest->val, &id_src->val);
     rtl_set_CF(&t1);
     
-    rtl_xor(&t1, &id_dest->val, &id_src->val);
+    rtl_slt(&t1, &id_dest->val, &t0);
+    rtl_set_OF(&t1);
+
+    /*rtl_xor(&t1, &id_dest->val, &id_src->val);
     rtl_xor(&t2, &id_dest->val, &t0);
     rtl_and(&t1, &t1, &t2);
     rtl_msb(&t1, &t1, id_dest->width);
     rtl_set_OF(&t1);
-
+    */
     print_asm_template1(dec);
 }
 
