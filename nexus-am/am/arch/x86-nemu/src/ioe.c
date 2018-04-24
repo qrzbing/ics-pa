54053c8d87f60f6ba48b26f28c298975971abe06
tracer-ics2017 <tracer@njuics.org>
2018-04-24 19:54:12 +0800

    >  run 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:48:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:54:12 up 32 min,  4 users,  load average: 0.10, 0.14, 0.35 8b81c19a489e2f2a8416a9fe604d0b013208fc57

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -31,7 +31,7 @@ void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
     //}
     for(i = y; i < y + _screen.height; ++i){
         for(j = x; j < x + _screen.width; ++j){
-            fb[i *_screen.width + j] = pixels[(i - y) * _screen.width + (j - x)];
+            fb[i * w + j] = pixels[(i - y) * w + (j - x)];
         }
     }
 }
