24b2494cc24df4ea3f641ce7cbb5cde65fa03560
tracer-ics2017 <tracer@njuics.org>
2018-04-24 19:32:19 +0800

    >  run 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:48:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:32:19 up 10 min,  4 users,  load average: 0.03, 0.77, 0.85 562ae3eb4f9ed06b7aec13dfa8702af7de423e5e

--- nexus-am/am/arch/x86-nemu/src/ioe.c
+++ nexus-am/am/arch/x86-nemu/src/ioe.c
@@ -23,12 +23,11 @@ extern void* memcpy(void *, const void *, int);
 
 void _draw_rect(const uint32_t *pixels, int x, int y, int w, int h) {
     int i;
-    int cp_bytes = sizeof(uint32_t) * w;
-    //for (i = 0; i < _screen.width * _screen.height; i++) {
-    for (i = 0; i < h; i++){
-        memcpy(&fb[(y + i) * w + x], pixels, cp_bytes);
-        //fb[i] = pixels[i];
-        pixels += w;
+    //int cp_bytes = sizeof(uint32_t) * w;
+    for (i = 0; i < _screen.width * _screen.height; i++) {
+    //for (i = 0; i < h; i++){
+        //memcpy(&fb[(y + i) * w + x], pixels, cp_bytes);
+        fb[i] = pixels[i];
     }
 }
 
