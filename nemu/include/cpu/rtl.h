06a6d3ffa224f1cb31540109ace0092287726353
tracer-ics2017 <tracer@njuics.org>
2018-05-02 18:39:45 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-38-generic #43~16.04.1-Ubuntu SMP Wed Mar 14 17:48:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  18:39:45 up  5:25,  3 users,  load average: 0.60, 1.12, 1.06 81c5314e9b9494e0c0b3ac2a902ccf758a37095

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -134,7 +134,7 @@ static inline void rtl_not(rtlreg_t* dest) {
     *dest = ~(*dest);   // Perhaps Wrong
 }
 
-void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
     //TODO();
     
