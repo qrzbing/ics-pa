0b9451d84fcb5e52af2437cb35ae795e3d654a97
tracer-ics2017 <tracer@njuics.org>
2018-05-23 16:53:04 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  16:53:04 up  2:21,  8 users,  load average: 1.74, 0.91, 0.57 78c0ec6e625af8385528ba25e187f06093e3add2

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,25 +7,15 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
 
     // TODO();
-    union {
-            GateDesc gd;
-            struct { uint32_t lo, hi;  };
-          
-    } item;
-      vaddr_t addr;
-
-      addr = 8 * NO + cpu.idtr.base;
-      item.lo = vaddr_read(addr, 4);
-      item.hi = vaddr_read(addr + 4, 4);
-
-      decoding.is_jmp = 1;
-      decoding.jmp_eip = (item.gd.offset_15_0 & 0xFFFF)
-        | ((item.gd.offset_31_16 & 0xFFFF) << 16);
-
-      t0 = cpu.cs;
-      rtl_push(&t0);
-      rtl_push(&cpu.eflags);
-      rtl_push(&decoding.seq_eip);
+    rtl_push(&cpu.eflags);
+        t0 = cpu.cs;
+        rtl_push(&t0);;
+        rtl_push(&ret_addr);
+        uint32_t target_eip_low, target_eip_high;
+        target_eip_low = vaddr_read(cpu.idtr.base + NO * 8, 4) & 0x0000ffff;
+        target_eip_high = vaddr_read(cpu.idtr.base + NO * 8 + 4, 4) & 0xffff0000;
+       decoding.is_jmp = 1;   
+       decoding.jmp_eip = target_eip_low | target_eip_high;
 }
 
 void dev_raise_intr() {
