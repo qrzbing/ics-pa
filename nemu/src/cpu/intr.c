4e2291a8f62de4fd80ab7cbf1742851fabe78bd7
tracer-ics2017 <tracer@njuics.org>
2018-05-23 17:30:05 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  17:30:05 up  2:58,  9 users,  load average: 0.07, 0.19, 0.38 fbcf99d870278112164a87c0e00926a19aea1e34

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,26 +7,25 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
 
     // TODO();
+    
+    t0 = cpu.cs;
+    rtl_push(&cpu.eflags);
+    rtl_push(&t0);
+    rtl_push(&ret_addr);
+    
+    vaddr_t addr = NO * 8 + cpu.idtr.base;
+    
     union {
-            GateDesc gd;
-            struct { uint32_t lo, hi;  };
-          
-    } item;
-      vaddr_t addr;
-
-      addr = 8 * NO + cpu.idtr.base;
-      item.lo = vaddr_read(addr, 4);
-      item.hi = vaddr_read(addr + 4, 4);
-
-    Log("%#x", cpu.idtr.base);  
+        GateDesc tempGate;
+        struct {
+            uint32_t low_val;
+            uint32_t high_val;
+        };
+    }temp;
+    temp.low_val = vaddr_read(addr, 4);
+    temp.high_val = vaddr_read(addr + 4, 4);
     decoding.is_jmp = 1;
-      decoding.jmp_eip = (item.gd.offset_15_0 & 0xFFFF)
-        | ((item.gd.offset_31_16 & 0xFFFF) << 16);
-
-      t0 = cpu.cs;
-      rtl_push(&t0);
-      rtl_push(&cpu.eflags);
-      rtl_push(&decoding.seq_eip);
+    decoding.jmp_eip = (temp.tempGate.offset_15_0 & 0xffff) | ((temp.tempGate.offset_31_16 & 0xffff) << 16);
 }
 
 void dev_raise_intr() {
