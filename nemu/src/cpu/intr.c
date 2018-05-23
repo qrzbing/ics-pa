7c777668919990e9f7e423841ae7756da9bf8509
tracer-ics2017 <tracer@njuics.org>
2018-05-23 14:46:16 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-41-generic #46~16.04.1-Ubuntu SMP Thu May 3 10:06:43 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  14:46:16 up 14 min,  3 users,  load average: 0.36, 1.04, 1.22 b7af5a6cbb2f9221f78910bfbbb2fae459dc142

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -6,7 +6,11 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    * That is, use ``NO'' to index the IDT.
    */
 
-  TODO();
+    TODO();
+    rtl_push(&cpu.eflags);
+    t0 = cpu.cs;
+    rtl_push(&t0);
+    rtl_push(&ret_addr);
 }
 
 void dev_raise_intr() {
