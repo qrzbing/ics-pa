74b152311ef48bb3fffd99e252ec5e914ee24571
tracer-ics2017 <tracer@njuics.org>
2018-06-06 02:54:10 +0800

    >  run 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  02:54:10 up  4:12, 10 users,  load average: 2.03, 1.78, 1.45 7b94273d1a78fd9dac762e19c2133fa7b12730a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -25,3 +25,83 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+int fs_open(const char *pathname, int flags, int mode){
+    int fd;
+    for(fd = 0; fd < NR_FILES; ++fd){
+        if(strcmp(pathname, file_table[fd].name) == 0){
+            break;
+        }
+    }
+    if(fd >= NR_FILES) panic("file not found!");
+    file_table[fd].open_offset = 0;
+    return fd;
+}
+
+void ramdisk_read(void *buf, off_t offset, size_t len);
+ssize_t fs_read(int fd, void *buf, size_t len){
+    
+    Finfo *fp = &file_table[fd];
+    
+    if(fp->size - fp->open_offset < len){
+        len = fp->size - fp->open_offset;
+    }
+
+    switch(fd){
+        case FD_STDIN: case FD_STDOUT: case FD_STDERR:
+            return -1;
+        default:
+            ramdisk_read(buf, fp->disk_offset + fp->open_offset, len);
+            fp->open_offset += len;
+        return len;
+    }
+}
+
+void ramdisk_write(const void *buf, off_t offset, size_t len);
+ssize_t fs_write(int fd, uint8_t *buf, size_t len){
+    
+    Finfo *fp = &file_table[fd];
+
+    if(fp->size - fp->open_offset < len){
+        len = fp->size - fp->open_offset;
+    }
+
+    size_t i = 0;
+    switch(fd){
+        case FD_STDIN: return -1;
+        case FD_STDOUT: case FD_STDERR:
+            while(i++ < len) _putc(*buf++);
+            return len;
+        default:
+            ramdisk_write(buf, fp->disk_offset + fp->open_offset, len);
+            fp->open_offset += len;
+            return len;
+    }
+}
+
+off_t fs_lseek(int fd, off_t offset, int whence){
+    
+    Finfo *fp = &file_table[fd];
+    
+    switch(whence){
+        case SEEK_SET:
+            fp->open_offset = offset;
+            break;
+        case SEEK_CUR:
+            fp->open_offset = fp->open_offset + offset;
+            break;
+        case SEEK_END:
+            fp->open_offset = fp->size + offset;
+            break;
+        default: return -1;
+    }
+    return fp->open_offset;
+}
+
+int fs_close(int fd){
+    return 0;
+}
+
+size_t fs_filesz(int fd){
+    return file_table[fd].size;
+}
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,4 +1,5 @@
 #include "common.h"
+#include "fs.h"
 
 extern size_t get_ramdisk_size();
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
@@ -6,7 +7,12 @@ extern void ramdisk_read(void *buf, off_t offset, size_t len);
 #define DEFAULT_ENTRY ((void *)0x4000000)
 uintptr_t loader(_Protect *as, const char *filename) {
     // TODO();
-    size_t len = get_ramdisk_size();
-    ramdisk_read(DEFAULT_ENTRY, 0, len);
+    // size_t len = get_ramdisk_size();
+    // ramdisk_read(DEFAULT_ENTRY, 0, len);
+    
+    int fd = fs_open(filename, 0, 0);
+    size_t len = fs_filesz(fd);
+    Log("LOAD [%d] %s. Size:%d", fd, filename, len);
+    fs_read(fd, DEFAULT_ENTRY, len);
     return (uintptr_t)DEFAULT_ENTRY;
 }
