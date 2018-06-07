ae8386e8a252cbdc9401e87d53531ddcf244b043
tracer-ics2017 <tracer@njuics.org>
2018-06-07 23:47:16 +0800

    >  compile U201414606 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:47:16 up 40 min,  1 user,  load average: 0.52, 0.67, 0.79 826db0dcab0d7d276453dfa42088967f63f1d1fd

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -112,12 +112,12 @@ static inline void rtl_sr(int r, int width, const rtlreg_t* src1) {
 }
 
 #define make_rtl_setget_eflags(f) \
-    static inline void concat(rtl_set_, f) (const rtlreg_t* src) { \
-        cpu.f = *src & 0x1; \
-    } \
-    static inline void concat(rtl_get_, f) (rtlreg_t* dest) { \
-        *dest = cpu.f & 0x1;\
-    }
+  static inline void concat(rtl_set_, f) (const rtlreg_t* src) { \
+    cpu.f = *src; \
+  } \
+  static inline void concat(rtl_get_, f) (rtlreg_t* dest) { \
+    *dest = cpu.f ? 1 : 0; \
+  }
 
 make_rtl_setget_eflags(CF)
 make_rtl_setget_eflags(OF)
@@ -126,85 +126,71 @@ make_rtl_setget_eflags(SF)
 
 static inline void rtl_mv(rtlreg_t* dest, const rtlreg_t *src1) {
   // dest <- src1
-    *dest = *src1;
+  *dest = *src1;
 }
 
 static inline void rtl_not(rtlreg_t* dest) {
   // dest <- ~dest
-    *dest = ~(*dest);   // Perhaps Wrong
+  *dest = ~*dest;
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
-    //TODO();
-    
-    //int32_t ttemp = (int32_t)(*src1);
-    //ttemp = ttemp << (32 - width * 8);
-    //ttemp = ttemp >> (32 - width * 8);
-    //*dest = ttemp;
-    //rtl_li(&t0, 32 - width * 8);
-    //rtl_shli(dest, src1, 32 - width * 8);
-    //rtl_sari(dest, src1, 32 - width * 8);
-    rtl_shli(dest, src1, 32 - width * 8);
-    rtl_sari(dest, dest, 32 - width * 8);
+  int32_t tmp = (int32_t)*src1;
+  tmp <<= 32 - (8 * width);
+  tmp >>= 32 - (8 * width);
+  *dest = tmp;
 }
 
 static inline void rtl_push(const rtlreg_t* src1) {
   // esp <- esp - 4
   // M[esp] <- src1
-    rtl_subi(&cpu.esp, &cpu.esp, 4);
-    rtl_sm(&cpu.esp, 4, src1);
+  cpu.esp -= 4;
+  vaddr_write(cpu.esp, 4, *src1);
 }
 
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-    rtl_lm(dest, &cpu.esp, 4);
-    rtl_addi(&cpu.esp, &cpu.esp, 4);
+  *dest = vaddr_read(cpu.esp, 4);
+  cpu.esp += 4;
 }
 
 static inline void rtl_eq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 == 0 ? 1 : 0)
-    *dest = (*src1 == 0);
+  *dest = (*src1 == 0);
 }
 
 static inline void rtl_eqi(rtlreg_t* dest, const rtlreg_t* src1, int imm) {
   // dest <- (src1 == imm ? 1 : 0)
-    *dest = (*src1 == imm);
+  *dest = (*src1 == imm);
 }
 
 static inline void rtl_neq0(rtlreg_t* dest, const rtlreg_t* src1) {
   // dest <- (src1 != 0 ? 1 : 0)
-    *dest = (*src1 != 0);
+  *dest = (*src1 != 0);
 }
 
 static inline void rtl_msb(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- src1[width * 8 - 1]
-
-  *dest = (*src1 >> (width * 8 - 1)) & 0x1;
+  *dest = (*src1 >> (8 * width - 1)) & 0x1;
 }
 
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
-    //unsigned int temp = ~0;
-    //temp = temp >> (32 - 8 * width);
-    //cpu.ZF = (*result & temp) | 0;
-    unsigned temp;
-    rtl_eq0(&temp, result);
-    rtl_set_ZF(&temp);
-    //cpu.ZF = temp & 0x1;
-    //cpu.ZF = (*result & ~(0xffffffff << (8 * width - 1) << 1)) == 0;
+  assert (width == 4 || width == 2 || width == 1);
+  cpu.ZF = (*result & ~(0xffffffff << (8 * width - 1) << 1)) == 0;
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
-    // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
-    cpu.SF = (*result >> (width * 8 - 1)) & 0x1;
+  // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
+  assert (width == 4 || width == 2 || width == 1);
+  cpu.SF = (*result >> (8 * width - 1)) & 0x1;
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
-    assert(width == 4 || width == 2 || width ==1);
-    rtl_update_ZF(result, width);
-    rtl_update_SF(result, width);
+  rtl_update_ZF(result, width);
+  rtl_update_SF(result, width);
 }
 
 #endif
