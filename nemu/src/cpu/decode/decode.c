d3582d4ff2fbb5177dc9aa2b7336d5857bc0146c
tracer-ics2017 <tracer@njuics.org>
2018-06-07 23:44:04 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:44:04 up 37 min,  1 user,  load average: 0.80, 0.72, 0.83 1ef46250cc105c256d7621dfb0458cbceea34796

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -38,11 +38,10 @@ static inline make_DopHelper(SI) {
    *
    op->simm = ???
    */
-    t0 = instr_fetch(eip, op->width); 
-    rtl_sext(&t0, &t0, op->width);
-    op->simm = t0;
-    rtl_li(&op->val, op->simm);
-    
+  op->imm = instr_fetch(eip, op->width);
+  rtl_sext(&op->val, &op->imm, op->width);
+  op->simm = op->val;
+
 #ifdef DEBUG
   snprintf(op->str, OP_STR_SIZE, "$0x%x", op->simm);
 #endif
