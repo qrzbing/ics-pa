a179ce63e572ebf5a4235a5b043d75e02bc62cdb
tracer-ics2017 <tracer@njuics.org>
2018-06-07 23:47:48 +0800

    >  run 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:47:48 up 40 min,  1 user,  load average: 0.62, 0.69, 0.79 cd0c3e31b5eebe451509c51c02b0708a4d5b96f

--- nemu/include/monitor/watchpoint.h
+++ nemu/include/monitor/watchpoint.h
@@ -7,19 +7,8 @@ typedef struct watchpoint {
   int NO;
   struct watchpoint *next;
 
-  /* TODO: Add more members if necessary */
-  char *expr;
-  uint32_t old;
-
-
+    char expression[80];
+    uint32_t value;
 } WP;
-
-void init_wp_pool();
-WP *new_wp();
-void free_wp(WP *);
-bool wp_has_changed();
-WP *find_wp(int);
-void print_wp();
-
 
 #endif
