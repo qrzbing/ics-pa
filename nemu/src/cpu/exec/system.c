4d65f689ba6c3599dc5b53f51d3101c8aa65e1ce
tracer-ics2017 <tracer@njuics.org>
2018-06-07 23:37:20 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  23:37:20 up 30 min,  1 user,  load average: 0.96, 0.73, 0.91 5c2a6865f006a3aafcb80c233509660f63d50f2b

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -1,18 +1,18 @@
 #include "cpu/exec.h"
 #include "memory/mmu.h"
+
+extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
 void diff_test_skip_qemu();
 void diff_test_skip_nemu();
-extern void raise_intr(uint8_t NO, vaddr_t ret_addr);
+
 make_EHelper(lidt) {
-    // TODO();
-    cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
-    if(decoding.is_operand_size_16){
-        cpu.idtr.base = vaddr_read(id_dest->addr + 2, 3);
-    }
-    else {
-        cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
-    }
-    print_asm_template1(lidt);
+  cpu.idtr.limit = vaddr_read(id_dest->addr, 2);
+  if (decoding.is_operand_size_16)
+    cpu.idtr.base = vaddr_read(id_dest->addr + 2, 3);
+  else
+    cpu.idtr.base = vaddr_read(id_dest->addr + 2, 4);
+
+  print_asm_template1(lidt);
 }
 
 make_EHelper(mov_r2cr) {
@@ -32,9 +32,9 @@ make_EHelper(mov_cr2r) {
 }
 
 make_EHelper(int) {
-    // TODO();
-    raise_intr(id_dest->val, decoding.seq_eip);
-    print_asm("int %s", id_dest->str);
+  raise_intr(id_dest->val, decoding.seq_eip);
+
+  print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
   diff_test_skip_nemu();
@@ -42,24 +42,25 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-    // TODO();
-    rtl_pop(&t0);
-    decoding.is_jmp = 1;
-    decoding.jmp_eip = t0;
-    rtl_pop(&t0);
-    cpu.eflags = t0;
-    rtl_pop(&t0);
-    cpu.cs = (uint16_t)t0;
-    print_asm("iret");
+  rtl_pop(&t0);
+  decoding.is_jmp = 1;
+  decoding.jmp_eip = t0;
+  rtl_pop(&t0);
+  cpu.eflags = t0;
+  rtl_pop(&t0);
+  cpu.cs = (uint16_t)t0;
+
+  print_asm("iret");
 }
 
 uint32_t pio_read(ioaddr_t, int);
 void pio_write(ioaddr_t, int, uint32_t);
 
 make_EHelper(in) {
-    t0 = pio_read(id_src->val, id_dest->width);
-    operand_write(id_dest, &t0);
-    print_asm_template2(in);
+  t1 = pio_read(id_src->val, id_dest->width);
+  operand_write(id_dest, &t1);
+
+  print_asm_template2(in);
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();
@@ -67,11 +68,9 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  //TODO();
-    //printf("%#x\n", id_dest->val);
-    pio_write(id_dest->val, id_dest->width, id_src->val);
-    //operand_write(id_dest, &id_src->val);
-    print_asm_template2(out);
+  pio_write(id_dest->val, id_dest->width, id_src->val);
+
+  print_asm_template2(out);
 
 #ifdef DIFF_TEST
   diff_test_skip_qemu();
