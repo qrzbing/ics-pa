04a694d497c467c4e030efe0fc859c526c43e604
tracer-ics2017 <tracer@njuics.org>
2018-06-18 20:30:53 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:30:53 up  5:55,  3 users,  load average: 1.33, 0.65, 0.45 7ec890f1d70938a574ea3bc3d1536ea1c4511248

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -16,13 +16,13 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-    Log("id-dest: %#x\nid-src: %#x", id_dest->val, id_src->val);
+    Log("id-dest: %#x\nid-src: %#x", id_dest->reg, id_src->reg);
     TODO();
     print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-    Log("id-dest: %#x\nid-src: %#x", id_dest->val, id_src->val);
+    Log("id-dest: %#x\nid-src: %#x", id_dest->reg, id_src->reg);
 
     TODO();    
     print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
