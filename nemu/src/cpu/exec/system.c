0c4193bfdf2148304a19282c585ce6472cdfbc56
tracer-ics2017 <tracer@njuics.org>
2018-06-18 20:23:12 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:23:11 up  5:47,  3 users,  load average: 0.64, 0.46, 0.34 ba54b7c27aad92ab864f2166f39b2b98f5c69d5e

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -16,18 +16,19 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-  TODO();
-
-  print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
+    Log("id-dest: %#x\nid-src: %#x", id_dest->val, id_src->val);
+    TODO();
+    print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-  TODO();
+    Log("id-dest: %#x\nid-src: %#x", id_dest->val, id_src->val);
 
-  print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
+    TODO();    
+    print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
-  diff_test_skip_qemu();
+    diff_test_skip_qemu();
 #endif
 }
 
@@ -37,7 +38,7 @@ make_EHelper(int) {
     print_asm("int %s", id_dest->str);
 
 #ifdef DIFF_TEST
-  diff_test_skip_nemu();
+    diff_test_skip_nemu();
 #endif
 }
 
@@ -62,18 +63,18 @@ make_EHelper(in) {
     print_asm_template2(in);
 
 #ifdef DIFF_TEST
-  diff_test_skip_qemu();
+    diff_test_skip_qemu();
 #endif
 }
 
 make_EHelper(out) {
-  //TODO();
+    //TODO();
     //printf("%#x\n", id_dest->val);
     pio_write(id_dest->val, id_dest->width, id_src->val);
     //operand_write(id_dest, &id_src->val);
     print_asm_template2(out);
 
 #ifdef DIFF_TEST
-  diff_test_skip_qemu();
+    diff_test_skip_qemu();
 #endif
 }
