f893652e2db10cdde4968510cbc2d795b44d42c0
tracer-ics2017 <tracer@njuics.org>
2018-06-18 19:52:32 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  19:52:32 up  5:16,  3 users,  load average: 0.80, 0.61, 0.42 3393da6a35f4f4b3ffeef5ea403a002b2ad7866f

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -30,10 +30,53 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
     }
 }
 
+paddr_t page_translate(vaddr_t addr, bool flag){
+    paddr_t paddr = addr;
+    /* only when protect mode and paging mode enable translate*/
+    if(cpu.cr0.protect_enable && cpu.cr0.paging){
+        /* initialize */
+        paddr_t pdeptr, pteptr;
+        PDE pde;
+        PTE pte;
+        /* find pde and read */
+        pdeptr = (paddr_t)(cpu.cr3.page_directory_base << 12) | (paddr_t)(((addr >> 22) & 0x3ff) * 4);
+        pde.val = paddr_read(pdeptr, 4);
+        assert(pde.present);
+        pde.accessed = 1;
+        /* find pte and read */
+        pteptr = (paddr_t)(pde.page_frame << 12) | (paddr_t)(((addr >> 12) & 0x3ff) * 4);
+        pte.val = paddr_read(pteptr, 4);
+        assert(pte.present);
+        pte.accessed = 1;
+        pte.dirty = (flag == true) ? 1 : 0;
+        /* find page and read */
+        paddr = (paddr_t)(pte.page_frame << 12) | (paddr_t)(addr & 0xfff);
+    }
+    return paddr;
+}
+
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  return paddr_read(addr, len);
+    bool flag = ((addr + len - 1) & (~PAGE_MASK)) != (addr & (~PAGE_MASK));
+    /* flag equals to true means data cross the page boundry */
+    if(flag == true){
+        /* this is a special case, you can handle it later. */
+        panic("data cross the page boundry");
+    }
+    else{
+        paddr_t paddr = page_translate(addr, false);
+        return paddr_read(paddr, len);
+    }
+    // return paddr_read(addr, len);
 }
 
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
-  paddr_write(addr, len, data);
+    bool flag = ((addr + len - 1) & (~PAGE_MASK)) != (addr & (~PAGE_MASK));
+    if(flag == true){
+        panic("data cross the page boundry");
+    }
+    else{
+        paddr_t paddr = page_translate(addr, true);
+        paddr_write(paddr, len, data);
+    }
+    // paddr_write(addr, len, data);
 }
