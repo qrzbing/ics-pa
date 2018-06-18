26f8a3096c3fa8400a0a0dcc3c9800729a7de836
tracer-ics2017 <tracer@njuics.org>
2018-06-18 20:40:19 +0800

    >  compile 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-43-generic #48~16.04.1-Ubuntu SMP Thu May 17 12:56:46 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  20:40:19 up  6:04,  3 users,  load average: 0.48, 0.63, 0.59 fb5a6408ebf24dd2fa4dd7075fb1c577a829d5ed

--- nemu/src/cpu/exec/system.c
+++ nemu/src/cpu/exec/system.c
@@ -16,15 +16,32 @@ make_EHelper(lidt) {
 }
 
 make_EHelper(mov_r2cr) {
-    Log("id-dest: %#x\nid-src: %#x", id_dest->reg, id_src->reg);
-    TODO();
+
+    switch(id_dest->reg){
+        case 0:
+            rtl_mv(&cpu.cr0.val, &id_src->val);
+            break;
+        case 3:
+            rtl_mv(&cpu.cr3.val, &id_src->val);
+            break;
+        default:
+            panic("unsupported control register");
+    }
     print_asm("movl %%%s,%%cr%d", reg_name(id_src->reg, 4), id_dest->reg);
 }
 
 make_EHelper(mov_cr2r) {
-    Log("id-dest: %#x\nid-src: %#x", id_dest->reg, id_src->reg);
-
-    TODO();    
+    switch(id_src->reg){
+        case 0:
+            rtl_mv(&t0, &cpu.cr0.val);
+            break;
+        case 3:
+            rtl_mv(&t0, &cpu.cr3.val);
+            break;
+        default:
+            panic("unsupported control register");
+    }
+    operand_write(id_dest, &t0);
     print_asm("movl %%cr%d,%%%s", id_src->reg, reg_name(id_dest->reg, 4));
 
 #ifdef DIFF_TEST
