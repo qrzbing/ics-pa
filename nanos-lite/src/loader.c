f3beadc34574154b1ecd202ad470629addaaad1d
tracer-ics2017 <tracer@njuics.org>
2018-06-24 03:12:48 +0800

    >  run 161620125 qrz Linux qrz-Inspiron-7559 4.13.0-45-generic #50~16.04.1-Ubuntu SMP Wed May 30 11:18:27 UTC 2018 x86_64 x86_64 x86_64 GNU/Linux  03:12:48 up  2:31,  3 users,  load average: 0.68, 0.83, 0.74 8d8b953c6a211213e2fe96643e9c27ed557fbd1

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,9 +18,11 @@ uintptr_t loader(_Protect *as, const char *filename) {
     void *fz_end = DEFAULT_ENTRY + len;
     void *va, *pa;
     for(va = DEFAULT_ENTRY; va < fz_end; va += PGSIZE){
+        
         pa = new_page();
         _map(as, va, pa);
         fs_read(fd, pa, (fz_end - va) < PGSIZE ? (fz_end - va) : PGSIZE);
+        Log("va: %#x, pa: %#x", va, pa);
     }
     // fs_read(fd, DEFAULT_ENTRY, len);
     return (uintptr_t)DEFAULT_ENTRY;
